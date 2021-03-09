9cde5057d9606fa7f70aa86c46fc4f4ebf80324b
Abhay <abhaygupta26@gmail.com>
2021-03-10 03:21:09 +0530

    Adding Edit page with edit and update action, adding show page with show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,28 @@
 class UsersController < ApplicationController
 
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your Profile was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+
   end
 
   def create
