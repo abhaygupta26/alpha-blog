ef88cc47ef1d984093383a45f7a359d3c52b7b7a
Abhay <abhaygupta26@gmail.com>
2021-03-12 01:19:12 +0530

    add login and log out successfully

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You have successfully loged in"
+      redirect_to user
+    else
+      flash.now[:alert] = "There is something wrong with your credentials"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Successfully Logged Out"
+    redirect_to root_path
   end
 
 end
