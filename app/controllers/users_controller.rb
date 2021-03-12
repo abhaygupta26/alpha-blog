89aad3cb002518906ccdccfc175be4198c6b2bf0
Abhay <abhaygupta26@gmail.com>
2021-03-13 02:24:31 +0530

    restrict actions for user as well as articles controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 before_action :set_user, only: [:show, :edit, :update]
+before_action :require_user, only: [:edit, :update]
+before_action :require_same_user, only: [:edit, :update]
 
   def show
     articles = @user.articles
@@ -48,4 +50,10 @@ before_action :set_user, only: [:show, :edit, :update]
     @user = User.find(params[:id])
   end
 
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own profile."
+      redirect_to @user
+    end
+  end
 end
