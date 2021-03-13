17579d8191e2e3c7c7f95807f0f8f688e289f30c
Abhay <abhaygupta26@gmail.com>
2021-03-13 22:19:54 +0530

    Adding admin user to our app and given the changing colour functionality of error and success msgs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:show, :edit, :update, :destroy]
-before_action :require_user, only: [:edit, :update]
-before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
-    articles = @user.articles
-    @articles = @user.articles.paginate(page: params[:page], per_page: 5)  end
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
 
   def index
-    @user = User.all
     @user = User.paginate(page: params[:page], per_page: 5)
   end
 
@@ -21,34 +20,32 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def update
     if @user.update(user_params)
-      flash[:notice] = "Your Profile was updated successfully"
+      flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
     else
       render 'edit'
     end
-
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to the Alpha Blog, #{@user.username}"
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
       render 'new'
     end
-
   end
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
-    flash[:notice] = "You have successfully deleted you account."
-    redirect_to root_path
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Your Account and all associated articles successfully deleted"
+    redirect_to articles_path
   end
 
   private
-
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
@@ -58,9 +55,10 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
   end
 
   def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can only edit your own profile."
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own account"
       redirect_to @user
     end
   end
+  
 end
